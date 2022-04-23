ee8809cac84ee009e999c8cc354a2749723642b6
Juan Romero <75763058+jromerooo2@users.noreply.github.com>
2022-04-22 21:38:21 -0600

    page for each user

---
+++ pages/advanced/profiles/[id].js
@@ -0,0 +1,11 @@
+import Layout from "../../../components/layout";
+
+const Details = () => {
+    return (
+        <Layout>
+        <h1>Details</h1>
+        </Layout>
+    );
+}
+
+export default Details;
--- pages/advanced/profiles/index.js
+++ pages/advanced/profiles/index.js
@@ -1,6 +1,6 @@
-import Layout from "../../components/layout"
-import auth0 from "../../lib/auth0"
-import data from "../../profiles.json"
+import Layout from "../../../components/layout"
+import auth0 from "../../../lib/auth0"
+import data from "../../../profiles.json"
 import Link from "next/link";
 
 function profiles({user}){
@@ -17,7 +17,7 @@ function profiles({user}){
                     {
                         dataUsers.map(user => (
                             <>
-                                <Link href="/profile/[id]" as={`/profile/${user.id}`} className="">
+                                <Link href={'/advanced/profiles/'+user.userId} >
                                     <div key={user.userId} className="p-2 md:my-9 my-6 card cursor-pointer">
                                         <div className={`bg-white rounded-lg shadow-xl p-4 ${user.exalumno ? "border border-yellow-500":""}`}>
                                             <span className={`rounded p-2 bg-red-500 text-white absolute -mt-8 ${user.exalumno ? "":"hidden"}`}>Recomendado</span>
