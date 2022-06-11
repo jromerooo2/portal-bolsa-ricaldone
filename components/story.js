c22d8e92276dbe8cd25a0bbee7496b532715997f
Juan Romero <75763058+jromerooo2@users.noreply.github.com>
2022-06-11 15:33:49 -0500

    getting data from bd and passing to component

--- components/story.js
+++ components/story.js
@@ -3,14 +3,38 @@ import Layout from './layout'
 
 
 export default function Story(props){
-  console.log(props);
+  let { storyData, user } = props;
+  console.log(storyData);
   return (
     <Layout>
         <h1 className='text-center'>
-            Perfiles Solicitados
+            Hola, {user.nameUser},este es un listado de todos los perfiles que has solicitado
         </h1>
-        <div className='md:flex justify-center items-center md:h-[600px]'>
-            
+        <div className='grid-cols-3 space-x-5 md:grid'>
+            {
+                storyData.map((story, index) => {
+                    return (
+                        <div className='' key={index}>
+                            <div className='px-8 pt-6 pb-8 mb-4 bg-green-200 rounded shadow-xl'>
+                                <div className='flex items-center justify-between'>
+                                    <div className='flex items-center'>
+                                        <img className='w-10 h-10 mr-4 rounded-full' src="https://media.discordapp.net/attachments/881990540860731402/984830320023064636/unknown.png" alt={story.Postulants.namePostulant + story.Postulants.lastName} />
+                                        <div className='text-sm'>
+                                            <h1 className='leading-none text-gray-900'>{story.Postulants.namePostulant} {story.Postulants.lastName}</h1>
+                                        </div>
+                                    </div>
+                                    <div className='flex items-center'>
+                                        <p className='text-xs text-gray-600'>{story.dateMod}</p>
+                                    </div>
+                                </div>
+                                <div className='text-base text-gray-800'>
+                                    <p>{story.requestedInfo}</p>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })
+            }
         </div>
     </Layout>
   )
--- pages/advanced/historial.js
+++ pages/advanced/historial.js
@@ -5,14 +5,17 @@ import React, {useEffect} from 'react';
 export default function historial() {
   let storyReturn;
   let [story, setStory] = React.useState([]);
+  let [user, setUser] = React.useState([]);
   useEffect(() => {
     
     const result = async () => {
       let data = await axios.get('/api/me');
-      let idUser = await data.data.data.responseBd.idUser;
-      let story = await axios.get('/api/story/' + idUser);
+      let user = await data.data.data.responseBd;
+      let story = await axios.get('/api/story/' + user.idUser);
       storyReturn = story.data;
       setStory(storyReturn);
+
+      setUser(user);
     }
     result();
   }, [])
@@ -20,7 +23,7 @@ export default function historial() {
 
   return (
     <>
-      <Story storyData={story} />
+      <Story storyData={story} user={user} />
     </>
   )
 }
