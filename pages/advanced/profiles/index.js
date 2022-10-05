667f39678f9eb2375a341c696c1ffe7c1751a6b0
Juan Romero <75763058+jromerooo2@users.noreply.github.com>
2022-10-05 09:43:59 -0500

    filter

--- pages/advanced/profiles/index.js
+++ pages/advanced/profiles/index.js
@@ -2,6 +2,7 @@ import Layout from "../../../components/layout"
 import Link from "next/link";
 import { useState, useEffect } from "react";
 import { PrismaClient } from "@prisma/client"
+import Image from "next/image";
 const prisma = new PrismaClient();
 
 
@@ -10,9 +11,9 @@ function profiles({profiles}){
     const usuario = false;
     const [dataFiltered, setFilter] = useState([]);   
     const [query, setQuery] = useState("");
-    
+
     const handleChange = () => {
-        return dataUsers.filter((el) => el.namePostulant.toLowerCase().includes(query.toLowerCase()));
+        return dataUsers.filter((el) => el.workSubjects.WorkSubject.toLowerCase().includes(query.toLowerCase()));
 
     }
     useEffect(() => {
@@ -22,40 +23,73 @@ function profiles({profiles}){
     return (
         <>
             <Layout user={usuario}>
-                <h1 className="p-8 text-3xl Font-bold text-center text-green-900">Perfiles Disponibles</h1>
+                <h1 className="p-8 text-3xl text-center text-green-900 Font-bold">Perfiles Disponibles</h1>
                 <p className="text-2xl text-center text-lime-900 Font">
-                    Bienvenido{usuario.nameUser}, aquí encontraras todos los perfiles disponibles para contratos. 
+                    Bienvenido {usuario.nameUser}, aquí encontraras todos los perfiles disponibles para contratos. 
                 </p>
-            <div class="flex text-center justify-center">
-            <div class="input-wrapper border-2">
-	                <input onChange={e => setQuery(e.target.value)} type="search" class="input" placeholder="Buscar por nombre" className="focus:border-0"/>
+            <div class="flex text-center justify-center align-center items-center space-x-4">
+                <h1 class="text-2xl text-center text-lime-900 Font">Buscar Por Area</h1>
+                <div class="input-wrapper">
+                <select className="p-2 mx-auto my-4 text-2xl text-center border-2 text-lime-900 Font" onChange={(e) => setQuery(e.target.value)}>
+                        <option value="ARQUITECTURA">ARQUITECTURA</option>
+                        <option value="DISEÑO GRAFICO">DISEÑO GRAFICO</option>
+                        <option value="ELECTROMECANICA">ELECTROMECANICA</option>
+                        <option value="SISTEMAS INFORMATICOS">SISTEMAS INFORMATICOS</option>
+                        <option value="AERONAUTICO">AERONAUTICO</option>
+                        <option value="ELECTRONICA">ELECTRONICA</option>
+                        <option value="HOTELERIA">HOTELERIA</option>
+                        <option value="MECÁNICA INDUSTRIAL">MECÁNICA INDUSTRIAL</option>
+                        <option value="MANTENIMIENTO AUTOMOTRIZ">MANTENIMIENTO AUTOMOTRIZ</option>
+                        <option value="SISTEMAS ELECTRICOS">SISTEMAS ELECTRICOS</option>
+                        <option value="ADMINISTRATIVO CONTABLE">ADMINISTRATIVO CONTABLE</option>
+                        <option value="SERVICIOS TURISTICOS">SERVICIOS TURISTICOS</option>
+                        <option value="DESARROLLO DE SOFTWARE">DESARROLLO DE SOFTWARE</option>
+                        <option value="CONTADURIA">CONTADURIA</option>
+                        <option value="SECRETARIADO">SECRETARIADO</option>
+                        <option value="SALUD">SALUD</option>
+                        <option value="OTRO">OTRO</option>
 
-                <svg xmlns="http://www.w3.org/2000/svg" class="input-icon" viewBox="0 0 20 20" fill="currentColor">
-                    <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
-                </svg>
+                </select>
+                </div>
             </div>
-            </div>
-                <div className="grid-cols-3 md:grid">
                     {
-                        dataFiltered.map(user => (
-                            <div key={user.idPostulant}>
-                                <Link href={'/advanced/profiles/'+user.idPostulant} >
-                                    <div key={user.userId} className="p-2 my-6 cursor-pointer md:my-9 card text-lime-900">
-                                        <div className={`bg-white rounded-lg shadow-xl p-4 ${user.Alumni ? "border border-yellow-500":""}`}>
-                                            <span className={`rounded p-2 bg-red-500 text-white absolute -mt-8 ${user.Alumni ? "":"hidden"}`}>Recomendado</span>
-                                            <img src={`data:image/jpeg;base64,${user.photoPostulant}`} alt={user.namePostulant} className="mx-auto rounded h-36" />
-                                            <h2 className="text-xl text-center font-bold pt-2.5 Font">{user.namePostulant +" "+ user.lastName}</h2>
-                                            <p className="text-lg text-center Font">{user.mailPostulant}</p>
-                                            {/* <p>{user.phoneNumber}</p>
-                                            <p>{user.emailAddress}</p> */}
-                                        </div>
-                                    </div>
-                                </Link>
-                            </div>                            
+                        dataFiltered.length > 0 ? (
+                            <div className="grid-cols-3 md:grid">
+                                {
+                                dataFiltered.map(user => (
+                                    <div key={user.idPostulant}>
+                                        <Link href={'/advanced/profiles/'+user.idPostulant} >
+                                            <div key={user.userId} className="p-2 my-6 cursor-pointer md:my-9 card text-lime-900">
+                                                <div className={`bg-white rounded-lg shadow-xl p-4 ${user.Alumni ? "border border-yellow-500":""}`}>
+                                                    <span className={`rounded p-2 bg-red-500 text-white absolute -mt-8 ${user.Alumni ? "":"hidden"}`}>Recomendado</span>
+                                                    <img src={`data:image/jpeg;base64,${user.photoPostulant}`} alt={user.namePostulant} className="mx-auto rounded h-36" />
+                                                    <h2 className="text-xl text-center font-bold pt-2.5 Font">{user.namePostulant +" "+ user.lastName}</h2>
+                                                    <p className="text-lg text-center Font">{user.mailPostulant}</p>
+                                                    {/* <p>{user.phoneNumber}</p>
+                                                    <p>{user.emailAddress}</p> */}
+                                                </div>
+                                            </div>
+                                        </Link>
+                                    </div>                            
 
-                        ))
+                                ))
+                                }
+                            </div>            
+
+                        ):
+                        (
+                            <div className="flex items-center justify-center m-5 mx-auto space-x-4 text-center align-center">
+                                    <div class=" rounded-lg border  p-8 text-center">
+                                        <h2 class="text-2xl font-medium">Lo sentimos...</h2>
+
+                                        <p class="mt-4 text-xl text-gray-500">
+                                            Aun no existen postulantes en esa area!.
+                                        </p>
+                                        <Image src='/void.png' alt='logo' width={220} height={220} />
+                                    </div>
+                            </div>
+                        )
                     }
-                </div>            
             </Layout>
         </>
     )
@@ -64,15 +98,10 @@ export async function getServerSideProps({ req, res }) {
 
     //Logic for db querying
     const profiles = await prisma.Postulants.findMany({
-        select: {
-            idPostulant: true,
-            namePostulant: true,
-            photoPostulant: true,
-            Alumni: true,
-            lastName: true,
-            mailPostulant: true
+          include: {         
+            workSubjects: true
           }
-    });
+            });
     //TODO: fetch state to show according to design
     //awaiting for the promise made up, use the id to
     //fetch states
